3ae3d0e6477e6263a346f2f22e1d7b8bc71c2ab1
Jean-Paul Da Cunha <dcjeanpaul@gmail.com>
2017-07-10 14:51:36 +0100

    timeline ADT

---
+++ modules/product-demo/product-demo-lang/src/main/java/com/liferay/product/demo/lang/EnUSResourceBundle.java
@@ -0,0 +1,29 @@
+package com.liferay.product.demo.lang;
+
+import java.util.Enumeration;
+import java.util.ResourceBundle;
+
+import org.osgi.service.component.annotations.Component;
+
+import com.liferay.portal.kernel.language.UTF8Control;
+
+@Component(
+	    property = { "language.id=en_US" }, 
+	    service = ResourceBundle.class
+	)
+public class EnUSResourceBundle extends ResourceBundle {
+	
+    @Override
+    protected Object handleGetObject(String key) {
+        return _resourceBundle.getObject(key);
+    }
+
+    @Override
+    public Enumeration<String> getKeys() {
+        return _resourceBundle.getKeys();
+    }
+
+    private final ResourceBundle _resourceBundle = ResourceBundle.getBundle(
+        "content.Language", UTF8Control.INSTANCE);
+
+}
